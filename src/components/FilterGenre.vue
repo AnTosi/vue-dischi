9498132646b9b33e0adef0e693a84950d2b185b0
AnTosi <andrea.tosi.uni@gmail.com>
2021-11-16 15:21:00 +0100

    compute genre

--- src/components/FilterGenre.vue
+++ src/components/FilterGenre.vue
@@ -1,22 +1,37 @@
 <template>
     <div>
-            <label for="filter-genre" class="px-2">Filter albums by genre:</label>
-            <select v-model="selected" @change="logSelected" name="filter-genre" id="filter-genre">
-                <option value="all">All</option>
-                <option value="rock">Rock</option>
-                <option value="jazz">Jazz</option>
-                <option value="pop">Pop</option>
-                <option value="metal">Metal</option>
-            </select>
-        </div>
+        <label for="filter-genre" class="px-2">Filter albums by genre:</label>
+        <select v-model="genre" @change="filterGenre" name="filter-genre" id="filter-genre">
+            <option value="all">All</option>
+            <option value="rock">Rock</option>
+            <option value="jazz">Jazz</option>
+            <option value="pop">Pop</option>
+            <option value="metal">Metal</option>
+        </select>
+        <p>
+            {{ filteredGenre }}
+        </p>
+    </div>
 </template>
 
 <script>
 export default {
-    methods: {
-        logSelected(){
-            console.log(this.selected);
-        }
+    
+    data() {
+        return {
+            genre: ""
+        };
+    },
+    computed: {
+        filteredGenre() {
+            return this.genre;
+        } 
     }
 }
 </script>
+
+<style lang="scss">
+    p {
+        color: white;
+    }
+</style>
